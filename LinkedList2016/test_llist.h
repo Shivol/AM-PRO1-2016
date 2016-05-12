85c9316680450aaed2bef25a67c3450c7cb11ce8
Ilya Loshkarev <loshkarev.i@gmail.com>
2016-05-12 17:16:03 +0300

    test list

--- LinkedList2016/test_llist.h
+++ LinkedList2016/test_llist.h
@@ -3,23 +3,7 @@
 // test_llist.h
 //
 #pragma once
-#include "llist.h"
 
-llist *get_array_list(const llist::datatype *array, size_t length)
-{
-    assert(array != nullptr);
-    tlist *arr_list = new llist[length];
-
-    for (size_t i = 0; i < length; i++) {
-        arr_list[i].data = array[i];
-        arr_list[i].next = &arr_list[i+1];
-        arr_list[i].prev = &arr_list[i-1];
-    }
-    arr_list[0].prev = nullptr;
-    arr_list[length - 1].next = nullptr;
-
-    return arr_list;
-}
 
 bool test_get_list_list();
 
